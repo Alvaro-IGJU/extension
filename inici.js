13d6b2293f502fb9480fa2dbe42ee54ac75e2cd1
Alvaro-IGJU <127084235+Alvaro-IGJU@users.noreply.github.com>
2023-12-15 16:56:47 +0100

    added amazon images

--- inici.js
+++ inici.js
@@ -1,5 +1,5 @@
 let tabId;
-chrome.tabs.query({ active: true, currentWindow: true } ,
+chrome.tabs.query({ active: true, currentWindow: true },
     function (tabs) {
         let currTab = tabs[0];
         if (currTab) { tabId = currTab.id }
@@ -16,54 +16,54 @@ btn.addEventListener("click", click_change_background);
 function click_change_background(evt) {
 
     chrome.scripting.executeScript({
-        target:{"tabId":tabId},
-        func:exec_change_background,
-        args:["red"]
+        target: { "tabId": tabId },
+        func: exec_change_background,
+        args: ["red"]
     });
 }
 
-function exec_change_background(valor){
-    document.getElementById("globalContainer").style.backgroundColor=valor;
+function exec_change_background(valor) {
+    document.getElementById("globalContainer").style.backgroundColor = valor;
 }
 
 
 //Change links
 function click_change_links(color) {
-    
+
     chrome.scripting.executeScript({
-        target:{"tabId":tabId},
-        func:exec_change_links,
-        args:[color]
+        target: { "tabId": tabId },
+        func: exec_change_links,
+        args: [color]
     });
 }
 
 
-document.getElementById("btn_change_links").addEventListener("click",()=>{
+document.getElementById("btn_change_links").addEventListener("click", () => {
     let color = document.getElementById("btn_change_links_colors").value;
     console.log(color)
     click_change_links(color);
 });
 
-function exec_change_links(valor){
+function exec_change_links(valor) {
     let links = document.getElementsByTagName("a");
     for (let i = 0; i < links.length; i++) {
-            links[i].style.color = valor
-        
+        links[i].style.color = valor
+
     }
 }
 
 //Delete images
 function click_delete_images() {
-    
+
     chrome.scripting.executeScript({
-        target:{"tabId":tabId},
-        func:exec_delete_images,
-        args:[]
+        target: { "tabId": tabId },
+        func: exec_delete_images,
+        args: []
     });
 }
 
-document.getElementById("btn_delete_imgs").addEventListener("click",()=>{
-   
+document.getElementById("btn_delete_imgs").addEventListener("click", () => {
+
     click_delete_images();
 });
 
@@ -79,103 +79,87 @@ function exec_delete_images() {
 //Show/Hide Passwords
 
 function click_changeState_passwords() {
-    
+
     chrome.scripting.executeScript({
-        target:{"tabId":tabId},
-        func:exec_changeState_passwords,
-        args:[]
+        target: { "tabId": tabId },
+        func: exec_changeState_passwords,
+        args: []
     });
 }
 
-document.getElementById("btn_changeState_passwords").addEventListener("click",()=>{
-   
+document.getElementById("btn_changeState_passwords").addEventListener("click", () => {
+
     click_changeState_passwords();
 });
 
 function exec_changeState_passwords() {
-    let allInputs = document.querySelectorAll('input'); 
-    allInputs.forEach(function(input) {
+    let allInputs = document.querySelectorAll('input');
+    allInputs.forEach(function (input) {
 
         if (input.getAttribute('type') === 'password') {
             input.setAttribute('type', 'text');
             input.setAttribute('is_pass', 'false');
-        }else if (input.getAttribute('type') === 'text' && input.getAttribute('is_pass') === "false"){
-           
-                input.setAttribute('type', 'password');
-                input.setAttribute('is_pass', 'true');
-            
+        } else if (input.getAttribute('type') === 'text' && input.getAttribute('is_pass') === "false") {
+
+            input.setAttribute('type', 'password');
+            input.setAttribute('is_pass', 'true');
+
         }
     });
 }
 
 //AMAZON
-let sticky_menu = document.createElement("div");
-sticky_menu.style.width = "200px";
-sticky_menu.style.height = "200px";
-sticky_menu.style.position = 'fixed';
-sticky_menu.style.top = '50%';
-sticky_menu.style.right = '10px';
-sticky_menu.style.backgroundColor = 'red';
-let button_images = document.createElement("button");
-button_images.textContent = "Show text images";
-button_images.id = "button_images_text";
-button_images.addEventListener("click",()=>{
-    
-    click_imagesText();
 
-});
 
-let button_prices = document.createElement("button");
-button_prices.textContent = "Search lowest price";
-button_prices.id = "button_lowest prices";
-sticky_menu.appendChild(button_images)
-sticky_menu.appendChild(button_prices)
-console.log(sticky_menu)
-function exec_showStickyMenu(sticky_menu){
-    console.log(sticky_menu)
-    document.body.appendChild(sticky_menu);
-}
 
-document.getElementById("btn_sticky_menu").addEventListener("click",()=>{
-    
-    click_showStickyMenu(sticky_menu);
-});
+function click_stickyMenu() {
 
-//show menu
-function click_showStickyMenu() {
-    console.log("AA")
     chrome.scripting.executeScript({
-        target:{"tabId":tabId},
-        func:exec_showStickyMenu,
-        args:[sticky_menu]
+        target: { "tabId": tabId },
+        func: exec_stickyMenu,
+        args: []
     });
 }
 
+document.getElementById("btn_sticky_menu").addEventListener("click", () => {
 
+    click_stickyMenu();
+});
 
-//show text images
-function click_imagesText() {
+function exec_stickyMenu() {
+    let sticky_menu = document.createElement("div");
+    sticky_menu.style.width = "200px";
+    sticky_menu.style.height = "200px";
+    sticky_menu.style.position = 'fixed';
+    sticky_menu.style.top = '50%';
+    sticky_menu.style.right = '10px';
+    sticky_menu.style.backgroundColor = 'red';
+    let button_images = document.createElement("button");
+    button_images.textContent = "Show text images";
+    button_images.id = "button_images_text";
+    button_images.addEventListener("click",()=>{
+        let images = document.getElementsByTagName("img");
+        let imagesArray = Array.from(images);
     
-    chrome.scripting.executeScript({
-        target:{"tabId":tabId},
-        func:exec_imagesText,
-        args:[]
-    });
+        for (let i = 0; i < imagesArray.length; i++) {
+            imagesArray[i].addEventListener("mouseover",(e)=>{
+               let alt =   imagesArray[i].getAttribute("alt");
+               //a-section a-spacing-mini _cDEzb_noop_3Xbw5 
+               let parent = imagesArray[i].parentElement;
+               let text = document.createElement("p");
+               text.style.position = "absolute"
+               text.style.top = e.clientY
+               text.style.left = e.clientX
 
-}
-
-    document.getElementById("button_images_text")
-
-
-function exec_imagesText() {
-    let images = document.getElementsByTagName("img");
-
-    let imagesArray = Array.from(images);
-
-    for (let i = 0; i < imagesArray.length; i++) {
-        imagesArray[i].addEventListener("mouseover", function(){
-            console.log(imagesArray[i].getAttribute("alt"));
-        });
-    }
-
+               text.textContent = alt;
+               parent.appendChild(text)
+            })
+        }
+    })    
+    let button_prices = document.createElement("button");
+    button_prices.textContent = "Search lowest price";
+    button_prices.id = "button_lowest prices";
+    sticky_menu.appendChild(button_images)
+    sticky_menu.appendChild(button_prices)
+    document.body.appendChild(sticky_menu)
 }
